9a2522a46de0b67b317de7d1a05f3bfeb92c903a
goesq <158624036+goesq@users.noreply.github.com>
2024-12-01 00:06:57 -0300

    .

--- backendex/controllers/authController.js
+++ backendex/controllers/authController.js
@@ -1,8 +1,9 @@
-const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
-// Função de registro
+
+// Função para registrar um novo usuário
 exports.register = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -10,26 +11,27 @@ exports.register = async (req, res) => {
 
     const user = new User({ username, password });
     await user.save();
-    res.status(201).json({ message: "Usuário registrado com sucesso!" });
+    res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
   } catch (error) {
     console.log(error);
 
-    res.status(500).json({ message: "Erro no registro", error });
+    res.status(500).json({ message: "Falha ao registrar usuário", error });
   }
 };
 
-// Função de login
+// Função de autenticação (login)
 exports.login = async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
+  
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(400).json({ message: "Credenciais inválidas" });
+    return res.status(400).json({ message: "Credenciais incorretas" });
   }
+
   try {
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
     res.status(200).json({ token });
   } catch (error) {
-    res.status(500).json({ message: "Erro no registro", error });
-
+    res.status(500).json({ message: "Erro ao gerar token", error });
   }
 };
